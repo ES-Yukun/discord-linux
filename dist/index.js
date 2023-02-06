ec1259187ee211353db18e6eaf115ed3755ba46b
SE-Yukun <yukun@anylinks.jp>
2023-02-07 02:25:25 +0900

    update

---
+++ dist/index.js
@@ -0,0 +1,82 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Discord = __importStar(require("discord.js"));
+const tslog_1 = require("tslog");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const console = new tslog_1.Logger();
+process.on("uncaughtException", (err) => console.error(err));
+const cli = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.DirectMessageReactions,
+        Discord.GatewayIntentBits.DirectMessageTyping,
+        Discord.GatewayIntentBits.DirectMessages,
+        Discord.GatewayIntentBits.GuildBans,
+        Discord.GatewayIntentBits.GuildEmojisAndStickers,
+        Discord.GatewayIntentBits.GuildIntegrations,
+        Discord.GatewayIntentBits.GuildInvites,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildMessageReactions,
+        Discord.GatewayIntentBits.GuildMessageTyping,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.GuildPresences,
+        Discord.GatewayIntentBits.GuildScheduledEvents,
+        Discord.GatewayIntentBits.GuildVoiceStates,
+        Discord.GatewayIntentBits.GuildWebhooks,
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.MessageContent,
+    ],
+    partials: [Discord.Partials.Channel],
+});
+cli.on("ready", () => __awaiter(void 0, void 0, void 0, function* () {
+    console.info("ready");
+}));
+cli.on("messageCreate", (msg) => __awaiter(void 0, void 0, void 0, function* () {
+    if (msg.author.bot)
+        return;
+    if (!msg.guild)
+        return;
+    if (msg.channel.type == Discord.ChannelType.GuildText) {
+        let res = msg.content.match(/(rm )([1-9]*[0-9])/);
+        if (res == null)
+            return;
+        let num = Number(res[2]);
+        if (num) {
+            msg.channel.bulkDelete(num);
+        }
+    }
+}));
+cli.login();
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import * as Discord from "discord.js";
+import { Logger } from "tslog";
+import { config } from "dotenv";
+
+config();
+const console = new Logger();
+process.on("uncaughtException", (err) => console.error(err));
+
+const cli = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.DirectMessageReactions,
+    Discord.GatewayIntentBits.DirectMessageTyping,
+    Discord.GatewayIntentBits.DirectMessages,
+    Discord.GatewayIntentBits.GuildBans,
+    Discord.GatewayIntentBits.GuildEmojisAndStickers,
+    Discord.GatewayIntentBits.GuildIntegrations,
+    Discord.GatewayIntentBits.GuildInvites,
+    Discord.GatewayIntentBits.GuildMembers,
+    Discord.GatewayIntentBits.GuildMessageReactions,
+    Discord.GatewayIntentBits.GuildMessageTyping,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.GuildPresences,
+    Discord.GatewayIntentBits.GuildScheduledEvents,
+    Discord.GatewayIntentBits.GuildVoiceStates,
+    Discord.GatewayIntentBits.GuildWebhooks,
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.MessageContent,
+  ],
+  partials: [Discord.Partials.Channel],
+});
+cli.on("ready", async () => {
+  console.info("ready");
+});
+cli.on("messageCreate", async (msg) => {
+  if (msg.author.bot) return;
+  if (!msg.guild) return;
+  if (msg.channel.type == Discord.ChannelType.GuildText) {
+    let res = msg.content.match(/(rm )([1-9]*[0-9])/);
+    if (res == null) return
+    let num = Number(res[2]);
+    if (num) {
+      msg.channel.bulkDelete(num);
+    }
+  }
+});
+cli.login();
